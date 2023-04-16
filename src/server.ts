1852e82c403b7784003fafc5fb9fc7b9599f384f
Antonin-Landeau <alandeau@charles-poncet.net>
2023-04-16 14:57:40 +0200

    add cors  origin

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ let users: any[] = [];
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://kart-manager-react.vercel.app"],
   },
 });
 
